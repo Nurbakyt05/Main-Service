4f9e95359cde2541508ce565c250802b9b065376
nurbakit <nurbakit1605@gmail.com>
2024-10-18 17:01:50 +0500

    first test commit GitHub

---
+++ src/main/java/kz/bitlab/MainServiceProject/MainServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.MainServiceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/bitlab/MainServiceProject/MainServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.MainServiceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
